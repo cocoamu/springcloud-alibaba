ff0a7250b57cc48f7ddf74b200713c423349d57b
huanglm <422943393@qq.com>
2022-05-31 16:16:57 +0800

    add feign demo

--- springcloud-client/src/main/java/com/cocoamu/cloud/SpringcloudClientApplication.java
+++ springcloud-client/src/main/java/com/cocoamu/cloud/SpringcloudClientApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * <h1>Nacos Client 工程启动入口</h1>
  * */
 //@ServletComponentScan
 //@EnableCircuitBreaker   // 启动 Hystrix
-//@EnableFeignClients
+@EnableFeignClients
 @RefreshScope   // 刷新配置
 @EnableDiscoveryClient
 @SpringBootApplication
